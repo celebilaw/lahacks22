1e83311ddabd8cc349b0fed7eff35034b3a31a3c
Kyle Pu <kylez.pu@gmail.com>
2022-04-16 14:32:41 -0700

    Refactor NavBar
    Move out request menu to PostRequests so we have much cleaner NavBar again.

--- friendinneed/src/components/NavBar.js
+++ friendinneed/src/components/NavBar.js
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -7,117 +6,19 @@ import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Link } from 'react-router-dom';
-import AddIcon from '@mui/icons-material/Add';
 import Stack from '@mui/material/Stack';
-import TextField from '@mui/material/TextField';
-import { MenuItem } from '@mui/material';
 import "../css/Navbar.css";
-import {db} from '../config.js';
-import {collection, addDoc, Timestamp} from 'firebase/firestore';
+import PostRequest from './PostRequest';
 
 const Navbar = () => {
-
-    let [item, setItem] = useState("");
-    let [description, setDescription] = useState("");
-    let [urgency, setUrgency] = useState("");
-    let [loc, setLoc] = useState("");
 
     const handleNewRequest = (e) => {
         document.getElementById("request-form").classList.toggle("show");
     }
 
-    const handleItemChange = (e) => {
-        setItem(e.target.value);
-    }
-
-    const handleDescChange = (e) => {
-        setDescription(e.target.value);
-    }
-
-    const handleUrgencyChange = (e) => {
-        setUrgency(e.target.value);
-    }
-
-    const handleLocChange = (e) => {
-        setLoc(e.target.value);
-    }
-
-    const handleSubmit = async (e) => {
-        // Do some checks to ensure no blank fields are submitted
-        if(item === "" || description === "" || loc === "" || urgency === ""){
-            return;
-        }
-
-        e.preventDefault();
-        const docRef = await addDoc(collection(db, "borrowrequests"), {
-            title: item,
-            desc: description,
-            location: loc,
-            requester: "sudoUser",//replace with auth user
-            posted: Timestamp.now(),
-            priority: urgency,
-            status: 0
-        });
-        console.log("Document written with ID: ", docRef.id);//TODO: REMOVE IN PROD
-
-        // Clear form elements when user submits
-        setItem("");
-        setUrgency("");
-        setLoc("");
-
-        // Close new request form when user clicks submit
-        document.getElementById("request-form").classList.toggle("show");
-    }
-
-    const handleCancel = (e) => {
-        // Clear form elements when user cancels
-        setItem("");
-        setUrgency("");
-        setLoc("");
-        document.getElementById("request-form").classList.toggle("show");
-    }
-
-    const urgencies = ["SOS", "Immediate", "Couple Hours", "Days", "Weeks"]
-    const landmarks = ["Powell", "YRL", "Sproul", "Delta Terrace", "Sunset Village", "Rieber", "De Neve", "Olympic", "Centennial", "Ackerman", "Court of Sciences", "Inverted Fountain"].sort() // Sort by alphabetical order
-
     return (
         <div>
-            <Stack
-                id="request-form"
-                component="form"
-                sx={{
-                    width: '25ch'
-                }}
-                spacing={0.5}
-                noValidate
-                autoComplete="off"
-                className="RequestForm"
-            >
-                <TextField label="Item" variant="outlined" value={item} onChange={handleItemChange} />
-                <TextField label="Description" variant="outlined" value={description} onChange={handleDescChange} />
-
-                <TextField
-                    id="UrgencyLabel"
-                    value={urgency}
-                    onChange={handleUrgencyChange}
-                    select
-                    label="Urgency">
-                    {urgencies.map(u => <MenuItem value={u} key={u}>{u}</MenuItem>)}
-                </TextField>
-
-                <TextField
-                    id="LocationLabel"
-                    value={loc}
-                    onChange={handleLocChange}
-                    select
-                    label="Place">
-                    {landmarks.map(place => <MenuItem value={place} key={place}>{place}</MenuItem>)}
-                </TextField>
-
-                <Button onClick={handleSubmit}>Submit Request</Button>
-                <Button onClick={handleCancel}>CANCEL</Button>
-            </Stack>
-
+            <PostRequest />
             <Box sx={{ flexGrow: 1 }}>
                 <AppBar position="static">
                     <Toolbar>
--- friendinneed/src/components/PostRequest.js
+++ friendinneed/src/components/PostRequest.js
@@ -1,71 +1,118 @@
-import {useState} from 'react';
-import {db} from '../config.js';
-import {collection, addDoc, Timestamp} from 'firebase/firestore';
-
-const requestTemplate = {
-  "title": "",
-  "desc": "",
-  "requester": "",
-  "status": null,
-  "priority": "",
-  "time": null
-};
+import { useState } from 'react'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { Link } from 'react-router-dom';
+import AddIcon from '@mui/icons-material/Add';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import { MenuItem } from '@mui/material';
+import "../css/Navbar.css";
+import { db } from '../config.js';
+import { collection, addDoc, Timestamp } from 'firebase/firestore';
 
 const PostRequest = () => {
-  // const [req, setReq] = useState(requestTemplate);
 
-  // const handleChange = ({target}) => {
-  //   setReq({
-  //     ...req,
-      
-  //   });
-  // };
-  const [title, setTitle] = useState("");
-  const [desc, setDesc] = useState("");
-  const [priority, setPriority] = useState("");
-  const [location, setLocation] = useState("");
+  let [item, setItem] = useState("");
+  let [description, setDescription] = useState("");
+  let [urgency, setUrgency] = useState("");
+  let [loc, setLoc] = useState("");
+
+  const handleItemChange = (e) => {
+    setItem(e.target.value);
+  }
+
+  const handleDescChange = (e) => {
+    setDescription(e.target.value);
+  }
+
+  const handleUrgencyChange = (e) => {
+    setUrgency(e.target.value);
+  }
+
+  const handleLocChange = (e) => {
+    setLoc(e.target.value);
+  }
 
   const handleSubmit = async (e) => {
+    // Do some checks to ensure no blank fields are submitted
+    if (item === "" || description === "" || loc === "" || urgency === "") {
+      return;
+    }
+
     e.preventDefault();
     const docRef = await addDoc(collection(db, "borrowrequests"), {
-      title: title,
-      desc: desc,
-      location: location,
+      title: item,
+      desc: description,
+      location: loc,
       requester: "sudoUser",//replace with auth user
       posted: Timestamp.now(),
-      priority: priority,
+      priority: urgency,
       status: 0
     });
     console.log("Document written with ID: ", docRef.id);//TODO: REMOVE IN PROD
+
+    // Clear form elements when user submits
+    setItem("");
+    setUrgency("");
+    setLoc("");
+
+    // Close new request form when user clicks submit
+    document.getElementById("request-form").classList.toggle("show");
   }
+
+  const handleCancel = (e) => {
+    // Clear form elements when user cancels
+    setItem("");
+    setUrgency("");
+    setLoc("");
+    document.getElementById("request-form").classList.toggle("show");
+  }
+
+  const urgencies = ["SOS", "Immediate", "Couple Hours", "Days", "Weeks"]
+  const landmarks = ["Powell", "YRL", "Sproul", "Delta Terrace", "Sunset Village", "Rieber", "De Neve", "Olympic", "Centennial", "Ackerman", "Court of Sciences", "Inverted Fountain"].sort() // Sort by alphabetical order
 
   return (
     <div>
-      <form onSubmit={handleSubmit} className='addTask' name='addTask'>
-        <input 
-          type='text' 
-          name='title' 
-          onChange={(e) => setTitle(e.target.value)} 
-          value={title}
-          placeholder='Enter title'/>
-        <textarea 
-          onChange={(e) => setDesc(e.target.value)}
-          placeholder='Enter task decription'
-          value={desc}></textarea>
-        <input 
-          type='text' 
-          name='location' 
-          onChange={(e) => setLocation(e.target.value)} 
-          value={location}
-          placeholder='Enter location'/>
-        <input 
-          type='text' 
-          name='priority' 
-          onChange={(e) => setPriority(e.target.value)} 
-          value={priority}
-          placeholder='enter priority'/>
-        <button type='submit'>Done</button>
-      </form> 
+      <Stack
+        id="request-form"
+        component="form"
+        sx={{
+          width: '25ch'
+        }}
+        spacing={0.5}
+        noValidate
+        autoComplete="off"
+        className="RequestForm"
+      >
+        <TextField label="Item" variant="outlined" value={item} onChange={handleItemChange} />
+        <TextField label="Description" variant="outlined" value={description} onChange={handleDescChange} />
+
+        <TextField
+          id="UrgencyLabel"
+          value={urgency}
+          onChange={handleUrgencyChange}
+          select
+          label="Urgency">
+          {urgencies.map(u => <MenuItem value={u} key={u}>{u}</MenuItem>)}
+        </TextField>
+
+        <TextField
+          id="LocationLabel"
+          value={loc}
+          onChange={handleLocChange}
+          select
+          label="Place">
+          {landmarks.map(place => <MenuItem value={place} key={place}>{place}</MenuItem>)}
+        </TextField>
+
+        <Button onClick={handleSubmit}>Submit Request</Button>
+        <Button onClick={handleCancel}>CANCEL</Button>
+      </Stack>
     </div>
   );
 }
