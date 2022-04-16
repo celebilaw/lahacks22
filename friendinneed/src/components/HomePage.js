8cbb1e8657ec9406febfcb04bb5e169d67911f2c
Kyle Pu <kylez.pu@gmail.com>
2022-04-16 16:16:03 -0700

    Refresh now interactively reflects DB updates

--- friendinneed/src/components/HomePage.js
+++ friendinneed/src/components/HomePage.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import { collection, getDocs, doc, updateDoc, deleteDoc, query, where } from "firebase/firestore";
-import {db} from '../config.js';
+import { db } from '../config.js';
 import "./HomePage.css"
 import Request from "./Request"
 
 function HomePage() {
-  
   const [borrowReqs, setBorrowReqs] = useState([])
 
   const cancelRequest = async (id) => {
     const docRef = doc(db, "borrowrequests", id);
     await deleteDoc(docRef);
+    window.location.reload(true);
   }
 
   const acceptRequest = async (id) => {
@@ -18,6 +18,7 @@ function HomePage() {
     await updateDoc(docRef, {
       status: 1
     });
+    window.location.reload(true);
   }
 
   const completeRequest = async (id) => {
@@ -25,8 +26,9 @@ function HomePage() {
     await updateDoc(docRef, {
       status: 2
     });
+    window.location.reload(true);
   }
-  
+
   useEffect(() => {
     async function fetchData() {
       const q = query(collection(db, "borrowrequests"), where("status", "!=", 2));
@@ -43,23 +45,25 @@ function HomePage() {
     <div>
       <div className="RequestCards">
         {borrowReqs.map((req) => (
-        <Request
-          id={req.id}
-          key={req.id}
-          item={req.data.item} 
-          description={req.data.description}
-          requester={req.data.requester}
-          fulfiller={req.data.fulfiller}
-          status={req.data.status}
-          urgency={req.data.urgency}
-          posted={req.data.posted}
-          location={req.data.location}
-          cancelRequest={cancelRequest}
-          acceptRequest={acceptRequest}
-          completeRequest={completeRequest}
-        />
-      ))}
+          <Request
+            id={req.id}
+            key={req.id}
+            item={req.data.item}
+            description={req.data.description}
+            requester={req.data.requester}
+            fulfiller={req.data.fulfiller}
+            status={req.data.status}
+            urgency={req.data.urgency}
+            posted={req.data.posted}
+            location={req.data.location}
+            cancelRequest={cancelRequest}
+            acceptRequest={acceptRequest}
+            completeRequest={completeRequest}
+          />
+        ))}
       </div>
+
+      <p>{document.cookie.split('; ').find(row => row.startsWith('username=')).split('=')[1]}</p>
     </div>
   );
 }
