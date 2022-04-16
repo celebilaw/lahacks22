326e75ae51bb4f163698ce4c7093691dafe4be01
celebilaw <celebilaw@gmail.com>
2022-04-15 23:20:48 -0700

    Created React Router and basic temporary components

--- friendinneed/src/App.js
+++ friendinneed/src/App.js
@@ -1,12 +1,30 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css';
+
+import UserProfile from "./components/user-profile.js";
+import MainPage from "./components/main-page.js";
+import Login from "./components/login.js";
+import Register from "./components/register.js";
+import Navbar from "./components/navbar.js";
 
 function App() {
   return (
-    <div>
-      <p>
-        Hi
-      </p>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          {/* 
+            probably do userprofile by ID? 
+            /userProfile/:id
+          */}
+          <Route path="/userProfile" element={<UserProfile />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
