2b011242045ef24ffeba491827a5a56536ba0bb8
Amanda Wang <52474776+theamandawang@users.noreply.github.com>
2022-04-16 09:38:27 -0700

    check for user on main

--- friendinneed/src/components/MainPage.js
+++ friendinneed/src/components/MainPage.js
@@ -1,9 +1,26 @@
-import HomePage from './HomePage'
+import HomePage from './HomePage';
+import { useState } from 'react';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
-
+const auth = getAuth();
 const MainPage = () => {
+    let [loggedin, setLoggedin] = useState(0);
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        const uid = user.uid;
+        setLoggedin(1);
+        // ...
+      } else {
+          
+      }
+    });
         return (
             <div>
+                <p>
+                    {loggedin ? 'yes i am logged in': 'no i am not'}
+                </p>
             <HomePage />
             </div>
         )
