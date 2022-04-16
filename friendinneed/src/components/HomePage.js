2a76095bad98403a4648c639f95c154bbee92d20
Kyle Pu <kylez.pu@gmail.com>
2022-04-16 11:16:03 -0700

    New Request Form done.
    Interactivity complete:
    
    - Grays out background and decreases opacity with transition
    - Can close the form either by clicking on SUBMIT REQUEST or by clicking outside of the form while it's in focus

--- friendinneed/src/components/HomePage.js
+++ friendinneed/src/components/HomePage.js
@@ -5,21 +5,21 @@ import AddIcon from '@mui/icons-material/Add';
 import { Button } from '@mui/material';
 import Stack from '@mui/material/Stack';
 import TextField from '@mui/material/TextField';
-import { Select, MenuItem, FormControl } from '@mui/material';
+import { MenuItem } from '@mui/material';
 
 function HomePage() {
 
   let [item, setItem] = useState("");
+  let [description, setDescription] = useState("");
   let [urgency, setUrgency] = useState("");
   let [loc, setLoc] = useState("");
 
-  const onClickAddRequest = () => {
-    let popup = document.getElementById("request-form");
-    popup.classList.toggle("show");
-  }
-
   const handleItemChange = (e) => {
     setItem(e.target.value);
+  }
+
+  const handleDescChange = (e) => {
+    setDescription(e.target.value);
   }
 
   const handleUrgencyChange = (e) => {
@@ -31,19 +31,41 @@ function HomePage() {
   }
 
   const handleSubmit = (e) => {
+    // Do some checks to ensure no blank fields are submitted
+
+
     console.log(item, urgency, loc);
+
+    // Clear form elements when user submits
     setItem("");
     setUrgency("");
     setLoc("");
+  
+    // Close new request form when user clicks submit
+    document.getElementById("request-form").classList.toggle("show");
+  }
+
+  const detectClick = (e) => {
+    let addIcon = document.getElementById("AddIcon");
+    let requestForm = document.getElementById("request-form");
+
+    // Close new request form IF it is open and user clicks outside of it
+    if(!requestForm.contains(e.target) && requestForm.classList.contains("show")){
+      document.getElementById("request-form").classList.toggle("show");
+    }
+
+    // Open new request form if add button is clicked
+    // This MUST be after the above check for closing the new form
+    if(addIcon.contains(e.target)){
+      document.getElementById("request-form").classList.toggle("show");
+    }
   }
 
   const urgencies = ["SOS", "Immediate", "Couple Hours", "Days", "Weeks"]
   const landmarks = ["Powell", "YRL", "Sproul", "Delta Terrace", "Sunset Village", "Rieber", "De Neve", "Olympic", "Centennial"].sort() // Sort by alphabetical order
 
   return (
-    <div>
-      <Button className="AddRequest" onClick={onClickAddRequest}><AddIcon /></Button>
-
+    <div onClick={detectClick}>
       <Stack
         id="request-form"
         component="form"
@@ -55,7 +77,8 @@ function HomePage() {
         autoComplete="off"
         className="RequestForm"
       >
-        <TextField label="Item" variant="outlined" value={item} onChange={handleItemChange}/>
+        <TextField label="Item" variant="outlined" value={item} onChange={handleItemChange} />
+        <TextField label="Description" variant="outlined" value={description} onChange={handleDescChange} />
 
         <TextField
           value={urgency}
@@ -76,9 +99,13 @@ function HomePage() {
         <Button onClick={handleSubmit}>Submit Request</Button>
       </Stack>
 
-      <Request title="Tweezers" desc="I need tweezers to pull out an ASUCLA food ticket I got stuck in my phone wallet. Thank you so much!" requester="Celebi Law" status="Fulfilled by Kyle Pu" />
-      <Request title="Tweezers" desc="I need tweezers to pull out an ASUCLA food ticket I got stuck in my phone wallet. Thank you so much!" requester="Celebi Law" status="Fulfilled by Kyle Pu" />
-      <Request title="Tweezers" desc="I need tweezers to pull out an ASUCLA food ticket I got stuck in my phone wallet. Thank you so much!" requester="Celebi Law" status="Fulfilled by Kyle Pu" />
+      <div className="RequestCards">
+        <Button className="AddRequest" id="AddIcon"><AddIcon /></Button>
+
+        <Request title="Tweezers" desc="I need tweezers to pull out an ASUCLA food ticket I got stuck in my phone wallet. Thank you so much!" requester="Celebi Law" status="Fulfilled by Kyle Pu" />
+        <Request title="Tweezers" desc="I need tweezers to pull out an ASUCLA food ticket I got stuck in my phone wallet. Thank you so much!" requester="Celebi Law" status="Fulfilled by Kyle Pu" />
+        <Request title="Tweezers" desc="I need tweezers to pull out an ASUCLA food ticket I got stuck in my phone wallet. Thank you so much!" requester="Celebi Law" status="Fulfilled by Kyle Pu" />
+      </div>
     </div>
   );
 }
